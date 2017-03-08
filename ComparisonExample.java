3d7b0d845e0cf79ccc9aab7df292b2897a23d630
Diego Serrano Gómez <diegosg448@gmail.com>
2017-03-07 23:28:00 -0600

    Add files via upload

---
+++ ComparisonExample.java
@@ -0,0 +1,11 @@
+public class ComparisonExample {
+
+    public static void main(String[] args) {
+        String a = "Hola";
+        if(a.equals("Hola")){
+            System.out.println("Igual");
+        }   
+        char b = a.charAt(0);
+        System.out.println(b);
+    }
+}
